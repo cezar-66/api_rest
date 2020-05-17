e89903fc7f3f2f0dacd1afcb5ef0c634819dfafd
cezar <cezar.marcal@softfocus.com.br>
2020-05-17 18:27:10 -0300

    projeto_api_rest

--- src/main/java/br/com/api_rest/ApiRestApplication.java
+++ src/main/java/br/com/api_rest/ApiRestApplication.java
@@ -2,9 +2,7 @@ package br.com.api_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/*
-	Autor : Cezar Marçal
- */
+
 @SpringBootApplication
 public class ApiRestApplication {
 
